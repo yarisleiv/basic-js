4af72534d27e3a84fbc597687fa7057d35acfcdc
Yaroslav Zhironkin <rainsolnce@gmail.com>
2020-10-31 19:10:20 +0300

    feat: implement function
    implement function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  'use strict';
+
+  if (typeof(sampleActivity) !== 'string' || !sampleActivity || !isFinite(sampleActivity) || +sampleActivity < 1 || +sampleActivity > 15) {
+    return false;
+  }
+
+  const k = Math.log(2) / HALF_LIFE_PERIOD;
+
+  const t = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
+
+  return t;
 };
