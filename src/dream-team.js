606f68b596d7b5cc7247b70bb22fb0b950512c97
Yaroslav Zhironkin <rainsolnce@gmail.com>
2020-10-31 19:12:09 +0300

    feat: implement function
     implement function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  'use strict';
+
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  const stringArray = members.filter((member) => typeof(member) === 'string');
+
+  let lettersArray = [];
+
+  stringArray.forEach((member) => {
+    lettersArray.push(member.match(/\b[a-zA-Z]/)[0].toUpperCase());
+  });
+
+  return lettersArray.sort().join('');
 };
