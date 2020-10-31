7bde4c2d2a73e6567cdb47338df2daed6e93027f
Yaroslav Zhironkin <rainsolnce@gmail.com>
2020-10-31 19:13:53 +0300

    feat: implement function
    implement function

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  'use strict';
+
+  const turns = 2 ** disksNumber - 1;
+  const seconds = Math.floor(((turns / turnsSpeed) * 3600));
+
+  return {turns, seconds};
 };
