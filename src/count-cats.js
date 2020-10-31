ff722b76e4301b11f82af2bcf8e0d4152d72441a
Yaroslav Zhironkin <rainsolnce@gmail.com>
2020-10-31 19:11:20 +0300

    feat: implement function
    implement function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  'use strict';
+
+  if (!matrix || !matrix[0]) {
+    return 0;
+  }
+
+  const x = matrix[0].length;
+  const y = matrix.length;
+
+  let result = 0;
+
+  for (let i = 0; i < y; i++) {
+    for (let j = 0; j < x; j++) {
+      if (matrix[i][j] === '^^') {
+        result++;
+      }
+    }
+  }
+
+  return result;
 };
